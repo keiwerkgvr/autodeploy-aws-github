c045aa52725db071d2f2028b8b5707e1cdb6ed77
Jose Villar <villar88@gmail.com>
2015-12-02 21:40:19 -0430

    testing

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <style>
+            body {
+                background-color: #d0e4fe;
+            }
+
+            h1 {
+                color: orange;
+                text-align: center;
+            }
+
+            p {
+                font-family: "Times New Roman";
+                font-size: 20px;
+            }
+        </style>
+    </head>
+    <body>
+
+        <h1>My First CSS Example</h1>
+        <p>This is a paragraph.</p>
+
+    </body>
+</html>
